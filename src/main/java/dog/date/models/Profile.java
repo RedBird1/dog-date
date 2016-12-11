b95113029bf609f89c367881e8bb9aab33c3e72f
Zachary Schleder <Zach@Zacharys-MacBook-Pro-3.local>
2016-12-10 18:59:11 -0600

    finished profile for now, uding dog for basic functionality

--- src/main/java/dog/date/models/Profile.java
+++ src/main/java/dog/date/models/Profile.java
@@ -2,6 +2,8 @@ package dog.date.models;
 
 public class Profile {
 	
+	private static final long serialVersionUID = 41L;
+	
 	private String city;
 	private String state;
 	private String zip;
@@ -9,4 +11,29 @@ public class Profile {
 	public Profile() {
 		
 	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	
 }
